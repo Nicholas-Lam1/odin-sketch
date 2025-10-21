a29a6022ce165ab96f1a80d899e58be540905548
Nicholas-Lam1 <nick40831@gmail.com>
2025-10-21 17:19:11 -0600

    Added in adjustable grid sizing

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let gridSize = 12;
 let isDrawing = false;
 let currentButton = 0;
 
@@ -16,6 +17,18 @@ function draw(e) {
   e.target.style.backgroundColor = (e.target.style.backgroundColor === "black" ? "white" : "black")
 }
 
+function inputSize() {
+  const sizeinput = document.getElementById("sizeinput")
+  const newSize = sizeinput.value;
+  if (!isNaN(newSize) && newSize > 0 && newSize <= 100) {
+    gridSize = newSize;
+    initGrid();
+  } else {
+    alert("Please enter a number between 1 and 100.");
+    sizeinput.value = gridSize;
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   (() => {
     console.log("Initializing app...");
@@ -25,22 +38,29 @@ document.addEventListener("DOMContentLoaded", () => {
   document.addEventListener("mousedown", (e) => {
     isDrawing = true;
     currentButton = e.buttons;
-    e.preventDefault(); 
   });
 
   document.addEventListener("mouseup", () => {
     isDrawing = false;
   });
 
-  document.addEventListener("contextmenu", (e) => e.preventDefault());
+  const drawgrid = document.getElementById("drawgrid");
+  drawgrid.addEventListener("contextmenu", (e) => e.preventDefault());
+
+  const sizeinput = document.getElementById("sizeinput");
+  sizeinput.value = gridSize
+
+  sizeinput.addEventListener("input", inputSize)
 });
 
 function initGrid() {
   const drawgrid = document.getElementById("drawgrid");
+  drawgrid.innerHTML = ""
   
-  for(var i = 0; i < 256; i++) {
+  for(var i = 0; i < gridSize ** 2; i++) {
     const drawbox = document.createElement("div");
     drawbox.classList.add("drawbox")
+    drawbox.style.flex = `0 0 calc(100%/${gridSize})`;
 
     drawbox.addEventListener("mouseenter", drag);
     drawbox.addEventListener("mousedown", draw);
